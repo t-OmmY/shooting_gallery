9dc43c21d2a8e200d276f5638b83f9206a232a08
t-OmmY <xperiask17ise@gmail.com>
2016-01-08 17:17:48 +0200

    Оптимизация Моделей, оптимизация контроллера

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -4,45 +4,28 @@ class AdminController extends Controller
 {
     public function indexAction()
     {
-        $infoModel = new infoModel();
-        $session_list = ($infoModel -> sessionList());
-        $templateDir = str_replace('Controller', '', __CLASS__);
-
-        // полный путь к шаблону содержит путь к папке View в виде константы VIEW_DIR
-        $templateFile = VIEW_DIR . $templateDir . DS . 'index.phtml';
-
-        // открываем буфер вывода, далее - подключение шаблона.
-        // там можно использовать переменные, которые определены в контроллере - с готовыми данными
-        ob_start();
-        require $templateFile;
-
-        // очистка буфера и возврат строки с динамческим контентом
-        return ob_get_clean();
+        $adminModel = new adminModel();
+        $session_list = ($adminModel -> sessionList());
+        $arg = array(
+            'session_list'=>$session_list
+        );
+        return $this -> render('index', $arg);
     }
 
     public function newSessionFormAction()
     {
-        $infoModel = new infoModel();
-        $for_new_session = $infoModel ->forNewSession();
-        $templateDir = str_replace('Controller', '', __CLASS__);
-
-        // полный путь к шаблону содержит путь к папке View в виде константы VIEW_DIR
-        $templateFile = VIEW_DIR . $templateDir . DS . 'new_session.phtml';
-
-        // открываем буфер вывода, далее - подключение шаблона.
-        // там можно использовать переменные, которые определены в контроллере - с готовыми данными
-        ob_start();
-        require $templateFile;
-
-        // очистка буфера и возврат строки с динамческим контентом
-        return ob_get_clean();
-
+        $adminModel = new adminModel();
+        $for_new_session = $adminModel ->forNewSession();
+        $arg = array(
+            'for_new_session'=>$for_new_session
+        );
+        return $this -> render('new_session', $arg);
     }
 
     public function addNewSessionAction()
     {
-        $updateModel = new updateModel();
-        $result = $updateModel->addSession($_POST);
+        $adminModel = new adminModel();
+        $result = $adminModel->addSession($_POST);
         if ( $result['status'] != 'Success'){
             return $this->render('error');
         } else {
@@ -55,27 +38,20 @@ class AdminController extends Controller
         if (is_object( $param )){
         $param = $param->post('session_id');
         }
-        $infoModel = new infoModel();
-        $for_new_serie = $infoModel ->forNewSerie($param);
-        $session_info = $infoModel->sessionInfo($param);
-        $templateDir = str_replace('Controller', '', __CLASS__);
-
-        // полный путь к шаблону содержит путь к папке View в виде константы VIEW_DIR
-        $templateFile = VIEW_DIR . $templateDir . DS . 'new_serie.phtml';
-
-        // открываем буфер вывода, далее - подключение шаблона.
-        // там можно использовать переменные, которые определены в контроллере - с готовыми данными
-        ob_start();
-        require $templateFile;
-
-        // очистка буфера и возврат строки с динамческим контентом
-        return ob_get_clean();
+        $adminModel = new adminModel();
+        $for_new_serie = $adminModel ->forNewSerie($param);
+        $session_info = $adminModel->sessionInfo($param);
+        $arg = array(
+            'for_new_serie'=>$for_new_serie,
+            'session_info'=>$session_info
+        );
+        return $this->render('new_serie', $arg);
     }
 
     public function addNewSerieAction()
     {
-        $updateModel = new updateModel();
-        $result = $updateModel->addSerie($_POST);
+        $adminModel = new adminModel();
+        $result = $adminModel->addSerie($_POST);
         if ( $result['status'] != 'Success'){
             return $this->render('error');
         } else {
@@ -89,194 +65,136 @@ class AdminController extends Controller
         if (is_object( $param )){
             $param=$_POST;
         }
-        $infoModel = new infoModel();
-        $session_info = $infoModel->sessionInfo($param['session_id']);
-        $serie_info = $infoModel->serieInfo($param['serie_id']);
-        $templateDir = str_replace('Controller', '', __CLASS__);
-
-        // полный путь к шаблону содержит путь к папке View в виде константы VIEW_DIR
-        $templateFile = VIEW_DIR . $templateDir . DS . 'new_hit.phtml';
-
-        // открываем буфер вывода, далее - подключение шаблона.
-        // там можно использовать переменные, которые определены в контроллере - с готовыми данными
-        ob_start();
-        require $templateFile;
-
-        // очистка буфера и возврат строки с динамческим контентом
-        return ob_get_clean();
-
+        $adminModel = new adminModel();
+        $session_info = $adminModel->sessionInfo($param['session_id']);
+        $serie_info = $adminModel->serieInfo($param['serie_id']);
+        $arg = array(
+            'session_info'=>$session_info,
+            'serie_info'=>$serie_info
+        );
+        return $this->render('new_hit', $arg);
     }
 
     public function addNewHitAction()
     {
-        $updateModel = new updateModel();
-        $result = $updateModel->addHits($_POST);
+        $adminModel = new adminModel();
+        $result = $adminModel->addHits($_POST);
         return ($this->selectSerieAction());
     }
 
     public function selectSessionAction(Request $request)
     {
         $param = $request->post('session_id');
-        $infoModel = new infoModel();
-        $session_info = $infoModel->sessionInfo($param);
-        $serie_list = $infoModel->serieList($param);
-
-        $templateDir = str_replace('Controller', '', __CLASS__);
-
-        // полный путь к шаблону содержит путь к папке View в виде константы VIEW_DIR
-        $templateFile = VIEW_DIR . $templateDir . DS . 'select_session.phtml';
-
-        // открываем буфер вывода, далее - подключение шаблона.
-        // там можно использовать переменные, которые определены в контроллере - с готовыми данными
-        ob_start();
-        require $templateFile;
-
-        // очистка буфера и возврат строки с динамческим контентом
-        return ob_get_clean();
-
+        $adminModel = new adminModel();
+        $session_info = $adminModel->sessionInfo($param);
+        $serie_list = $adminModel->serieList($param);
+        $arg = array(
+            'session_info'=>$session_info,
+            'serie_list'=>$serie_list
+        );
+        return $this->render('select_session', $arg);
     }
 
     public function selectSerieAction()
     {
-        $infoModel = new infoModel();
+        $adminModeloModel = new adminModel();
         if (!isset($_POST['serie_id'])){
-            $result = $infoModel->serieIdBySessionIdAndNubmer($_POST['session_id'], $_POST['serie_number']);
+            $result = $adminModeloModel->serieIdBySessionIdAndNubmer($_POST['session_id'], $_POST['serie_number']);
             $param = $result['result']['serie_id'];
         } else {
             $param = $_POST['serie_id'];
         }
-        $serie_info = $infoModel->serieInfo($param);
-        $hit_list = $infoModel->hitList($param);
-        $templateDir = str_replace('Controller', '', __CLASS__);
-
-        // полный путь к шаблону содержит путь к папке View в виде константы VIEW_DIR
-        $templateFile = VIEW_DIR . $templateDir . DS . 'select_serie.phtml';
-
-        // открываем буфер вывода, далее - подключение шаблона.
-        // там можно использовать переменные, которые определены в контроллере - с готовыми данными
-        ob_start();
-        require $templateFile;
-
-        // очистка буфера и возврат строки с динамческим контентом
-        return ob_get_clean();
-
-
+        $serie_info = $adminModeloModel->serieInfo($param);
+        $hit_list = $adminModeloModel->hitList($param);
+        $arg = array(
+            'serie_info'=>$serie_info,
+            'hit_list'=>$hit_list
+        );
+        return $this->render('select_serie', $arg);
     }
 
     public function deleteSessionAction()
     {
-        $updateModel = new updateModel();
-        $updateModel->deleteSession($_POST);
+        $adminModel = new adminModel();
+        $adminModel->deleteSession($_POST);
         return ($this->indexAction());
     }
 
     public function deleteSerieAction()
     {
-        $updateModel = new updateModel();
-        $updateModel->deleteSerie($_POST);
+        $adminModel = new adminModel();
+        $adminModel->deleteSerie($_POST);
         return ($this->selectSessionAction(new Request()));
     }
 
     public function deleteHitAction()
     {
-        $updateModel = new updateModel();
-        $updateModel->deleteHit($_POST);
+        $adminModel = new adminModel();
+        $adminModel->deleteHit($_POST);
         return ($this->selectSerieAction());
     }
 
     public function selectHitAction()
     {
-        $infoModel = new infoModel();
-        $hit_info = $infoModel->hitInfo($_POST['hit_id']);
-
-        $templateDir = str_replace('Controller', '', __CLASS__);
-
-        // полный путь к шаблону содержит путь к папке View в виде константы VIEW_DIR
-        $templateFile = VIEW_DIR . $templateDir . DS . 'select_hit.phtml';
-
-        // открываем буфер вывода, далее - подключение шаблона.
-        // там можно использовать переменные, которые определены в контроллере - с готовыми данными
-        ob_start();
-        require $templateFile;
-
-        // очистка буфера и возврат строки с динамческим контентом
-        return ob_get_clean();
+        $adminModel = new adminModel();
+        $hit_info = $adminModel->hitInfo($_POST['hit_id']);
+        $arg = array(
+            'hit_info'=>$hit_info
+        );
+        return $this->render('select_hit', $arg);
     }
 
     public function editSessionFormAction()
     {
-        $infoModel = new infoModel();
-        $for_new_session = $infoModel ->forNewSession();
-        $templateDir = str_replace('Controller', '', __CLASS__);
-
-        // полный путь к шаблону содержит путь к папке View в виде константы VIEW_DIR
-        $templateFile = VIEW_DIR . $templateDir . DS . 'edit_session.phtml';
-
-        // открываем буфер вывода, далее - подключение шаблона.
-        // там можно использовать переменные, которые определены в контроллере - с готовыми данными
-        ob_start();
-        require $templateFile;
-
-        // очистка буфера и возврат строки с динамческим контентом
-        return ob_get_clean();
+        $adminModel = new adminModel();
+        $for_new_session = $adminModel ->forNewSession();
+        $arg = array(
+            'for_new_session'=>$for_new_session
+        );
+        return $this->render('edit_session', $arg);
     }
 
     public function updateSessionAction()
     {
-        $updateModel = new updateModel();
-        $result = $updateModel ->editSession($_POST);
+        $adminModel = new adminModel();
+        $result = $adminModel ->editSession($_POST);
         return ($this->selectSessionAction(new Request()));
     }
 
     public function editSerieFormAction()
     {
 
-        $infoModel = new infoModel();
-        $session_list = $infoModel->sessionList();
-        $for_new_serie = $infoModel ->forNewSerie($_POST['session_id']);
-        $templateDir = str_replace('Controller', '', __CLASS__);
-
-        // полный путь к шаблону содержит путь к папке View в виде константы VIEW_DIR
-        $templateFile = VIEW_DIR . $templateDir . DS . 'edit_serie.phtml';
-
-        // открываем буфер вывода, далее - подключение шаблона.
-        // там можно использовать переменные, которые определены в контроллере - с готовыми данными
-        ob_start();
-        require $templateFile;
-
-        // очистка буфера и возврат строки с динамческим контентом
-        return ob_get_clean();
+        $adminModel = new adminModel();
+        $session_list = $adminModel->sessionList();
+        $for_new_serie = $adminModel ->forNewSerie($_POST['session_id']);
+        $arg = array(
+            'session_list'=>$session_list,
+            'for_new_serie'=>$for_new_serie
+        );
+        return $this->render('edit_serie', $arg);
     }
 
     public function updateSerieAction()
     {
-        $updateModel = new updateModel();
-        $result = $updateModel ->editSerie($_POST);
+        $adminModel = new adminModel();
+        $result = $adminModel ->editSerie($_POST);
         return ($this->selectSerieAction());
     }
 
     public function editHitFormAction()
     {
-        $infoModel = new infoModel();
-        $serie_list = $infoModel->serieIdList();
-        $templateDir = str_replace('Controller', '', __CLASS__);
-
-        // полный путь к шаблону содержит путь к папке View в виде константы VIEW_DIR
-        $templateFile = VIEW_DIR . $templateDir . DS . 'edit_hit.phtml';
-
-        // открываем буфер вывода, далее - подключение шаблона.
-        // там можно использовать переменные, которые определены в контроллере - с готовыми данными
-        ob_start();
-        require $templateFile;
-
-        // очистка буфера и возврат строки с динамческим контентом
-        return ob_get_clean();
+        $adminModel = new adminModel();
+        $serie_list = $adminModel->serieIdList();
+        $arg = array(
+            'serie_list'=>$serie_list
+        );
+        return $this->render('edit_hit', $arg);
     }
 
     public function updateHitAction()
     {
-        $updateModel = new updateModel();
-        $result = $updateModel->editHit($_POST);
+        $adminModel = new adminModel();
+        $result = $adminModel->editHit($_POST);
         return ($this->selectHitAction());
     }
 }
---
+++ Model/adminModel.php
@@ -0,0 +1,254 @@
+<?php
+
+class adminModel
+{
+    public static function select($query, $style = 'fetchAll')
+    {
+        $status = 'Success';
+
+        try {
+            $db = DbConnection::getInstance()->getPDO();
+
+            $sth = $db->query($query);
+            $result = $sth->$style(PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            $status = 'Fail: ' . $e->getMessage();
+        }
+
+        return array(
+            'status' => $status,
+            'result' => $result
+        );
+    }
+
+    public function sessionList()
+    {
+        return self::select('SELECT session_id, date, session_name FROM sessions ORDER BY date DESC');
+    }
+
+    public function serieList($param)
+    {
+        return self::select('SELECT number, name FROM series WHERE session_id='.$param);
+    }
+
+    public function serieIdList()
+    {
+        return self::select('SELECT serie_id, number, name FROM series ORDER BY serie_id');
+    }
+
+    public function hitList($param)
+    {
+        return self::select('SELECT hit_id FROM hits WHERE serie_id='.$param);
+    }
+
+    public function forNewSession()
+    {
+        $shooter_info = self::select('SELECT nickname, first_name, last_name FROM shooters');
+        $caliber_info = self::select('SELECT name as caliber_name FROM calibers');
+        $target_info = self::select('SELECT name as target_name FROM targets');
+        return array(
+            'shooter_info'=>$shooter_info['result'],
+            'caliber_info'=>$caliber_info['result'],
+            'target_info'=>$target_info['result']
+        );
+    }
+
+    public function forNewSerie($param)
+    {
+        $number = self::select('SELECT count(*) as number FROM series WHERE session_id='.$param, 'fetch');
+        $color_info = self::select('SELECT color_name FROM colors');
+        $scope_info = self::select('SELECT scope_name FROM scope');
+        $firesyle_info = self::select('SELECT firestyle_name FROM firestyle');
+        return array(
+            'color_info'=>$color_info['result'],
+            'number'=>$number['result']['number'] + 1,
+            'firestyle_info'=>$firesyle_info['result'],
+            'scope_info'=>$scope_info['result'],
+            'session_id'=>$param
+        );
+    }
+
+    public function sessionInfo($param)
+    {
+        return self::select('SELECT s.session_id, s.date, s.session_name, sh.nickname as shooter, t.name as target, c.name as caliber FROM sessions s JOIN calibers c ON s.caliber_id=c.caliber_id JOIN shooters sh ON sh.shooter_id = s.shooter_id JOIN targets t ON s.target_id=t.target_id WHERE session_id='.$param, 'fetch');
+    }
+
+    public function serieInfo($param)
+    {
+        return self::select('SELECT s.session_id, s.serie_id, s.number, c.color_name as \'color\', s.range, sc.scope_name as \'scope\', f.firestyle_name as \'firestyle\', s.name, s.comment FROM series s JOIN firestyle f ON f.firestyle_id=s.firestyle_id JOIN colors c ON c.color_id=s.color_id JOIN scope sc ON sc.scope_id=s.scope_id WHERE serie_id='.$param, 'fetch');
+    }
+
+    public function hitInfo($param)
+    {
+        return self::select('SELECT * FROM hits WHERE hit_id='.$param, 'fetch');
+    }
+
+    public function serieIdBySessionIdAndNubmer($session_id, $number)
+    {
+        return self::select('SELECT serie_id FROM series WHERE session_id='.$session_id.' AND number='.$number, 'fetch');
+    }
+
+    public function prepare($query, $param = array())
+    {
+        $status = 'Success';
+        try {
+            $db = DbConnection::getInstance()->getPDO();
+
+            $sth = $db->prepare($query);
+            $sth->execute($param);
+        } catch (PDOException $e) {
+            $status = 'Fail: ' . $e->getMessage();
+        }
+        return $status;
+
+    }
+
+    public function deleteSession($param)
+    {
+        return self::prepare('DELETE FROM sessions WHERE session_id=:session_id', $param);
+    }
+
+    public function deleteSerie()
+    {
+        return self::prepare('DELETE FROM series WHERE serie_id='.$_POST['serie_id']);
+    }
+
+    public function deleteHit()
+    {
+        return self::prepare('DELETE FROM hits WHERE hit_id='.$_POST['hit_id']);
+    }
+
+    public function editHit($param)
+    {
+        return self::prepare('UPDATE hits SET
+          serie_id=:serie_id,
+          x=:x,
+          y=:y
+          WHERE hit_id=:hit_id
+', $param);
+    }
+
+    public function editSerie($param)
+    {
+        return self::prepare('UPDATE series SET
+      color_id=(SELECT color_id FROM colors WHERE color_name=:color),
+      `range`=:range,
+      scope_id=(SELECT scope_id FROM scope WHERE scope_name=:scope),
+      firestyle_id=(SELECT firestyle_id FROM firestyle WHERE firestyle_name=:firestyle),
+      `name`=:serie_name,
+      comment=:serie_comment,
+      `number`=:serie_number,
+      session_id=:session_id
+      WHERE serie_id=:serie_id
+      ', $param);
+    }
+
+    public function editSession($param)
+    {
+        return self::prepare('UPDATE sessions SET
+                                 date=:date,
+                                 session_name=:session_name,
+                                 target_id=(SELECT target_id FROM targets WHERE name=:target),
+                                 shooter_id=(SELECT shooter_id FROM shooters WHERE first_name=:shooter),
+                                 caliber_id=(SELECT caliber_id FROM calibers WHERE name=:caliber)
+                                 WHERE session_id=:session_id
+                                ', $param);
+    }
+
+    public function addSession($param)
+    {
+        $status = 'Success';
+        $session_id = null;
+
+        try {
+            $db = DbConnection::getInstance()->getPDO();
+
+            $sth = $db->prepare('INSERT INTO sessions VALUES (
+      null,
+      :date,
+      :session_name,
+      (SELECT target_id FROM targets WHERE name=:target),
+      (SELECT shooter_id FROM shooters WHERE nickname=:shooter),
+      (SELECT caliber_id FROM calibers WHERE name=:caliber)
+      )');
+            $sth->execute($param);
+            $session_id = $db->lastInsertId();
+
+        } catch (PDOException $e) {
+            $status = 'Fail: ' . $e->getMessage();
+        }
+        return array(
+            'status'=>$status,
+            'session_id'=>$session_id
+        );
+    }
+
+
+
+    public function addSerie($params)
+    {
+        $status = 'Success';
+        $serie_id = null;
+
+        try {
+            $db = DbConnection::getInstance()->getPDO();
+
+            $sth = $db->prepare ('INSERT INTO series VALUES (
+      null,
+      (SELECT color_id FROM colors WHERE color_name=:color),
+      :range,
+      (SELECT scope_id FROM scope WHERE scope_name=:scope),
+      (SELECT firestyle_id FROM firestyle WHERE firestyle_name=:firestyle),
+      :serie_name,
+      :serie_comment,
+      :serie_number,
+      :session_id
+      )');
+            $sth->execute($params);
+            $serie_id = $db->lastInsertId();
+
+        } catch (PDOException $e) {
+            $status = 'Fail: ' . $e->getMessage();
+        }
+
+
+        return array(
+            'status'=>$status,
+            'serie_id'=>$serie_id,
+            'session_id'=>$params['session_id']
+        );
+    }
+
+
+    public function addHits($param)
+    {
+        $status = 'Success';
+        $serie_id = $param['serie_id'];
+        unset($param['serie_id']);
+        unset($param['session_id']);
+        try {
+            $db = DbConnection::getInstance()->getPDO();
+
+            foreach ($param as $hit){
+                $sth = $db->prepare ('INSERT INTO hits VALUES (
+          null,
+          '.$serie_id.',
+          :x,
+          :y
+)');
+                $sth->execute($hit);
+            }
+
+            $sth = $db->query('SELECT session_id FROM series WHERE serie_id='.$serie_id);
+            $result = ($sth->fetch(PDO::FETCH_ASSOC));
+        } catch (PDOException $e) {
+            $status = 'Fail: ' . $e->getMessage();
+        }
+        return array(
+            'status'=>$status,
+            'session_id'=>$result['session_id']);
+    }
+}
+
+
